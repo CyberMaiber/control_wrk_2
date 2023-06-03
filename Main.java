9bee3b32960de9f698015aefa7e7796cca605680
Dmitry Sharkov <89603380989@mail.ru>
2023-06-04 00:44:46 +0300

    added some comments

--- Main.java
+++ Main.java
@@ -6,14 +6,17 @@ package control_wrk_2;
 public class Main {
     public static void main(String[] args) {
         Seed ourSeed = new Seed();
-        try (Counter counter1 = new Counter(ourSeed.count)){
-            counter1.addAnimal("Barsik", "12.12.2018", "cat", ourSeed);
+        try (Counter counter1 = new Counter(ourSeed.count)){ //сделано через .опу чтоб прикрутить try-with-resourses
+            counter1.addAnimal("Barsik", "12.12.2018", "cat", ourSeed); //добавляем котика
+            //counter1.addAnimal("Barsik", "1212.2018", "cat", ourSeed); //ошибка в дате
+            //counter1.addAnimal("Barsik", "12.12.2018", "cat1", ourSeed); // ошибка в типе
+            counter1.addAnimal("Ia", "12.12.2018", "donkey", ourSeed); //добавляем ослика
         } catch (CloseCounterException e) {
             System.out.println(e.getMessage());
         } catch (AddingErrorCounterException e) {
             System.out.println(e.getMessage());
         }
-        ourSeed.printSeed();
+        ourSeed.printSeed(); //распечатываем зверинец
     }
 
 }
